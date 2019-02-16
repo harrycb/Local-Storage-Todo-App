b72c43ca29b36719ace0ef3f1af14f240dd5a244
Harry Courtenay-Bishop <harry.courtenay-bishop@loopup.com>
2019-02-16 14:31:31 +0000

    first push

---
+++ script.js
@@ -0,0 +1,202 @@
+
+// Variables used by script.
+// We need references to the textarea, the button and the div to place the results in
+var inputFormContainer = document.querySelector('.input-form'),
+    userInput = document.getElementById('user-input'),
+    saveButton = document.getElementById('btn'),
+    resultEl = document.getElementById('saved-text'),
+    resultElContent,
+    output = document.querySelector('.output'),
+    statusMsg = document.createElement('p'),
+    edit = document.getElementById('edit'),
+    editBox = document.createElement('textarea'),
+    saveEdit = document.createElement('button');
+
+// render stored notes
+resultEl.innerHTML = localStorage.getItem("toDoList");
+
+function storeLocally (){
+    resultElContent = resultEl.innerHTML;
+    localStorage.setItem("toDoList", resultElContent);
+}
+
+
+var totalItemsHtml = '<p>Total: <span id="total-items">0</span></p>';
+inputFormContainer.insertAdjacentHTML('beforeEnd' , totalItemsHtml);
+
+var totalItemsNumber =  document.getElementById('total-items');
+
+function totalItems (){
+    // space = child none so minus 1
+    var totalItems = resultEl.childNodes.length -1;
+    totalItemsNumber.textContent = totalItems;
+
+}
+totalItems ();
+
+
+// Helper function to help keep code clean...
+// All it does is make a button element, which we then add to each of the p 
+// elements we add to the page
+function makeMasterDeleteButton() {
+    var btn = document.createElement('a');
+    btn.innerHTML = '<span class="edit">Edit</span> X';
+    btn.classList.add('item-delete'); // give button the class "item-delete"
+    btn.setAttribute('href', '#');
+    return btn;
+}
+
+
+// Make sure we have valid elements to work with
+if (userInput !== null && saveButton !== null && resultEl !== null) {
+
+    // Listen for clicks on form button
+    saveButton.addEventListener('click', function (event) {
+
+        var newNode, // Element we will create
+            itemDeleteButton, // Delete button for element
+            submitted = userInput.value; // Text from form field
+
+        // Check if user entered something
+        if (submitted !== '') {
+
+            // Make a p element and add the submitted text to it
+            newNode = document.createElement('p');
+            newNode.textContent = submitted;
+
+            // Make a delete button element and append it to p element
+            itemDeleteButton = makeMasterDeleteButton();
+            newNode.insertAdjacentElement('afterbegin', itemDeleteButton);
+
+            // Status Message after fired before added
+            output.insertAdjacentElement('beforeEnd',statusMsg);
+            statusMsg.textContent = 'List item added';
+
+
+            // add the p element to the page
+            resultEl.appendChild(newNode);
+
+            // Erase user's text from the textarea
+            userInput.value = '';
+
+
+            window.setTimeout( function(){
+                statusMsg.remove();
+            } , 1000);
+
+
+        }
+
+        // Stop the browser's default behaviour. By default, when a "button" within a "form"
+        // element is clicked, the form gets submitted, resulting in a page reload (which we don't want)
+        event.preventDefault();
+
+        //add to local storage
+        storeLocally ();
+        //reset total
+        totalItems ();
+
+    });
+
+    // Add event listener for item deletions here... // you can use the same event for edit as well.
+    resultEl.addEventListener('click', outputContainer);
+
+
+}
+
+// Remove item function
+// But also edit
+function outputContainer(event){
+    event.preventDefault();
+    // Stop parent being
+    event.stopPropagation();
+
+    if(event.target.classList.contains('item-delete')){
+        event.preventDefault();
+
+        // Status Message after fired before removed
+        output.insertAdjacentElement('beforeEnd',statusMsg);
+        statusMsg.textContent = 'List item removed';
+
+        resultEl.removeChild(event.target.parentNode);
+
+        // remove edit box if no notes - 1 node = white space
+        //if(resultEl.childNodes.length == 1){
+        //to fix bug, just delete whole edit box if list item removed
+            editBox.remove();
+            saveEdit.remove();
+        //}
+
+        storeLocally ();
+        totalItems ();
+
+        window.setTimeout( function(){
+            statusMsg.remove();
+        } , 1000);
+
+    }
+
+    else if(event.target.classList.contains('edit')){
+
+        //Get parent of parent
+        var editTextContent = event.target.parentNode.parentNode;
+
+        //Text to remove
+        var removeText = 'Edit X';
+        var trimmedEditTextContent = editTextContent.textContent.replace(removeText , '');
+        console.log(trimmedEditTextContent);
+
+        output.insertAdjacentElement('beforeEnd',editBox);
+        editBox.id = 'edit-box';
+        output.insertAdjacentElement('beforeEnd',saveEdit);
+        saveEdit.textContent = 'Save edit';
+        saveEdit.id = 'save-edit';
+        editBox.textContent = trimmedEditTextContent;
+
+        //save edit controls
+        var noteControls = event.target.parentNode.parentNode;
+        noteControls = noteControls.innerHTML;
+
+        saveEdit.addEventListener('click' , saveEditFunction);
+
+        function saveEditFunction (){
+            event.preventDefault();
+            event.stopPropagation();
+
+            //Reset value of edit box
+            //editBox.value = '';
+            //remove edit box
+            editBox.remove();
+            saveEdit.remove();
+
+            // Status Message after edited
+            output.insertAdjacentElement('beforeEnd',statusMsg);
+            statusMsg.textContent = 'List item edited';
+
+            window.setTimeout( function(){
+                statusMsg.remove();
+            } , 1000);
+
+            function returnVal (){
+                event.target.parentNode.parentNode.innerHTML =  '<a class="item-delete" href="#"><span class="edit">Edit</span> X</a>' + editBox.value;
+                // reset value
+                editBox.textContent = trimmedEditTextContent;
+                storeLocally ();
+                totalItems ();
+            }
+
+            //place  edit controls
+            return returnVal ();
+
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
